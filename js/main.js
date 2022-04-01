3f6d803ccb3d136731f1b9b8d78d8f5636866244
vasylzayachkivskyi <vasylzaya@gmail.com>
2022-04-01 13:09:08 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   // ----------- start welcomescreen slider --------- //
   var swiper = new Swiper(".welcomescreen__slider", {
     scrollbar: {
@@ -9,35 +8,34 @@ $(document).ready(function () {
       nextEl: ".welcomescreen__slider-next",
       prevEl: ".welcomescreen__slider-prev",
     },
-    speed: 1000,
+    speed: 1200,
+    autoplay: {
+      delay: 1200,
+    },
   });
   // ----------- end welcomescreen slider --------- //
-
-
 
   // --- start burger menu --- //
   $(".burgerbtn").on("click", function () {
     $(this).toggleClass("active");
-    $('.mobile__menu').toggleClass("active");
+    $(".mobile__menu").toggleClass("active");
   });
- // --- end burger menu --- //
+  // --- end burger menu --- //
 
+  // --- start PARALLAX ----- //
+  if ($("body *").is("#scene")) {
+    var scene = document.getElementById("scene");
+    var parallaxInstance = new Parallax(scene);
+  }
+  // --- end PARALLAX ----- //
 
+  // --- start AOS ----- //
+  AOS.init({
+    duration: 1200,
+    easing: "ease",
+    disable: "mobile",
+  });
+  // --- end AOS ----- //
 
-// --- start PARALLAX ----- //
- if ($("body *").is("#scene")) {
-  var scene = document.getElementById("scene");
-  var parallaxInstance = new Parallax(scene);
- }
-// --- end PARALLAX ----- //
-
-// --- start AOS ----- //
- AOS.init({
-  duration: 1200,
-  easing: 'ease',
-  disable: 'mobile'
- });
-// --- end AOS ----- //
-
-
+  $("select").styler();
 });
