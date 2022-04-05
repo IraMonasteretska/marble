6b3747fafdb7a129bf117aea1abe2a6e0ef9cb25
IraMonasteretska <ira.monasteretska@gmail.com>
2022-04-05 14:40:43 +0300

    add we provide pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
   // ----------- start welcomescreen slider --------- //
   var swiper = new Swiper(".welcomescreen__slider", {
     scrollbar: {
@@ -43,15 +44,45 @@ $(document).ready(function () {
   $("select").styler();
   // --- end styleselect ----- //
 
+  // ----------- start welcomescreen slider --------- //
+  var swiper = new Swiper(".weprovideslider__inner", {
 
-    // --- start video player ----- //
-    const player = new Plyr(".js-player");
+    navigation: {
+      nextEl: ".weprovideslider-next",
+      prevEl: ".weprovideslider-prev",
+    },
+    slidesPerView: 3,
+    speed: 1200,
+    breakpoints: {
+      // when window width is >= 1600px
+      1600: {
+        spaceBetween: 40
+      },
+       // when window width is >= 1600px
+       1400: {
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      },
+      320: {
+        spaceBetween: 20,
+        slidesPerView: 1,
+      }
+    }
+  });
+  // ----------- end welcomescreen slider --------- //
 
-    $(".antietchvideo__content-btn").on("click", function () {
-      player.togglePlay();
-      $(this).toggleClass('pause');
-      
-    });
-    // --- end video player ----- //
+
+  // --- start video player ----- //
+  const player = new Plyr(".js-player");
+
+  $(".antietchvideo__content-btn").on("click", function () {
+    player.togglePlay();
+    $(this).toggleClass('pause');
+
+  });
+  // --- end video player ----- //
 
 });
